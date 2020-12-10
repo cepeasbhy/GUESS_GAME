e57452009da886947d94b64ce2e1221fe814043e
cepeasbhy <asbhycepe01@gmail.com>
2020-12-10 09:47:07 +0800

    Added user input for guessing the number and conditon statement for win and loss

--- guess.java
+++ guess.java
@@ -18,6 +18,21 @@ public class guess {
 			System.out.println("The computer will chose a number from 1 - 100");
 			System.out.println("YOu have 5 tries to guess the number");
 			System.out.println("---------------------------------------------");
+			
+			int rand_num = rand.nextInt(100)+1;
+			
+			for(int i = 1; i<=5; i++) {
+				System.out.print(i+": Enter your guess: ");
+				int guess_num = input.nextInt();
+				
+				if(guess_num == rand_num) {
+					System.out.println("YOU HAVE WON THE GAME!");
+					win++;
+				}else {
+					System.out.println("You have loss the game :(");
+					loss++;
+				}
+			}
 		}
 			
 	}
