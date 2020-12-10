9c1a37655318a69a7a4be6c0cdc3167e9d551562
cepeasbhy <asbhycepe01@gmail.com>
2020-12-10 09:42:02 +0800

    Added random generating code

--- guess.java
+++ guess.java
@@ -1,9 +1,25 @@
 
+import java.util.Scanner;
+import java.util.Random;
+
 public class guess {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		Random rand = new Random();
+		
+		int game_num = 0, win = 0, loss = 0;
+		
+		System.out.println("GUESSING GAME PROGRAM");
+		System.out.println("---------------------");
+		
+		while(true) {
+			System.out.println("---------------------------------------------");
+			System.out.println("The computer will chose a number from 1 - 100");
+			System.out.println("YOu have 5 tries to guess the number");
+			System.out.println("---------------------------------------------");
+		}
+			
 	}
 
 }
